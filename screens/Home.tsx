4f5f7d981c13dba1cf0ab71ba746542fc780b05e
James Tseng <58379166+jtfa22@users.noreply.github.com>
2023-01-21 12:38:16 -0800

    react native gifted charts

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -5,6 +5,18 @@ import { RootTabScreenProps } from '../types';
 import { View } from "react-native";
 import { Flex, Box, Surface, Spacer, VStack, Text } from "@react-native-material/core";
 
+// https://gifted-charts.web.app/barchart
+import { BarChart } from "react-native-gifted-charts";
+const barData = [
+    {value: 250, label: 'M'},
+    {value: 500, label: 'T', frontColor: '#177AD5'},
+    {value: 745, label: 'W', frontColor: '#177AD5'},
+    {value: 320, label: 'T'},
+    {value: 600, label: 'F', frontColor: '#177AD5'},
+    {value: 256, label: 'S'},
+    {value: 300, label: 'S'},
+];
+
 export default function Home({ navigation }: RootTabScreenProps<'Home'>) {
     return (
         <Flex>
@@ -14,9 +26,15 @@ export default function Home({ navigation }: RootTabScreenProps<'Home'>) {
                 {/* FAV COMP */}
                 <Surface elevation={2} style={{ marginHorizontal: 15, marginBottom: 15, width: 'auto', height: 'auto', borderRadius: 5 }}>
                     <VStack m={4} spacing={6}>
-                        <View style={{ width: 40, height: 40, backgroundColor: "#faf089" }} />
-                        <View style={{ width: 40, height: 40, backgroundColor: "#ff6347" }} />
-                        <View style={{ width: 40, height: 40, backgroundColor: "#fed7e2" }} />
+                    <BarChart
+                        barWidth={22}
+                        noOfSections={3}
+                        barBorderRadius={4}
+                        frontColor="lightgray"
+                        data={barData}
+                        yAxisThickness={0}
+                        xAxisThickness={0}
+                    />
                     </VStack>
                 </Surface>
                 
