ad3c4654ba653fcc2a9c804e7284597a0405087f
James Tseng <58379166+jtfa22@users.noreply.github.com>
2023-01-21 02:00:13 -0800

    add firebase config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,13 +3,13 @@ import '@firebase/auth';
 import '@firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'YOUR_KEY_HERE_AIzaSyAOWH',
-  authDomain: 'your-auth-domain-b1234.firebaseapp.com',
-  databaseURL: 'https://your-database-name.firebaseio.com',
-  projectId: 'your-project-id-1234',
-  storageBucket: 'your-project-id-1234.appspot.com',
-  messagingSenderId: '12345-insert-yourse',
-  appId: 'insert yours: 1:1234:web:ee873bd1234c0deb7eba61ce',
+  apiKey: 'AIzaSyBEfGe7v4AhjttRxx4hoG2KSQaAyw9L84Y',
+  authDomain: 'swiffed-idea-hacks-2023.firebaseapp.com',
+  databaseURL: 'https://swiffed-idea-hacks-2023-default-rtdb.firebaseio.com/',
+  projectId: 'swiffed-idea-hacks-2023',
+  storageBucket: 'swiffed-idea-hacks-2023.appspot.com',
+  messagingSenderId: '486038085197',
+  appId: '1:486038085197:ios:2fda4e625aed336e044604',
 };
 
 if (!firebase.apps.length) {
